ca6d66ab1790ce0b4b166b85c8e0a7db41772705
Kim-giseok <belbeck@naver.com>
2025-03-25 12:47:10 +0900

    [Feat] UISlot스크립트 작성

---
+++ Assets/Scripts/UI/UISlot.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISlot : MonoBehaviour
+{
+    [SerializeField] Image icon;
+
+    public void SetItem()
+    {
+
+    }
+
+    public void RefreshUI()
+    {
+            
+    }
+}
