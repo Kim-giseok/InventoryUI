43e58b983f745d9f71cc66f835b8d3a02ebdec05
Kim-giseok <belbeck@naver.com>
2025-03-25 10:56:17 +0900

    [Feat] UIManager업그레이드
    메니저를 싱글톤화 시키고, 각 ui들을 프로퍼티를 통해서 접근합니다.

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    static UIManager instance;
+    public static UIManager Instance => instance;
+
     [SerializeField] UIMainMenu mainMenu;
     [SerializeField] UIStatus status;
     [SerializeField] UIInventory inventory;
+
+    public UIMainMenu MainMenu => mainMenu;
+    public UIStatus Status => status;
+    public UIInventory Inventory => inventory;
 }
